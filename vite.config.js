ebff10eb520d308320aad390dd6ce22b932b30de
crosshj <github@chimpjuice.com>
2023-12-17 07:13:35 -0500

    node-polyfills

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	optimizeDeps: {
-		esbuildOptions: {
-			plugins: [
-				nodePolyfills(),
-				//
-			],
-		},
-	},
+	plugins: [react(), nodePolyfills()],
 });
